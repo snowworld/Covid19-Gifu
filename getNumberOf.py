4f549e4c14bfe91a03720566a9ae257099a88cad
yuki <snow14field@gmail.com>
2020-12-28 22:08:42 +0900

    modified

--- getNumberOf.py
+++ getNumberOf.py
@@ -2,10 +2,10 @@ import numberOfInfected, numberOfDead, numberOfInspected, numberOfGeneral, numbe
 
 def getNumberOf(userText):
 
-    if userText == "最新の感染者数は？": stat = f"最新の感染者数は { numberOfInfected.numberOfInfected() } 件です。"
-    elif userText == "最新の死亡者数は？": stat = f"最新の死亡者数は { numberOfDead.numberOfDead() } 件です。"
-    elif userText == "最新の検査数は？": stat = f"最新の検査数は { numberOfInspected.numberOfInspected() } 件です。"
-    elif userText == "最新の健康相談件数は？": stat = f"最新の健康相談件数は { numberOfGeneral.numberOfGeneral() } 件です。"
-    elif userText == "最新のコロナ相談数は？": stat = f"最新のコロナ相談件数は { numberOfCovid.numberOfCovid() } 件です。"
+    if userText == "最新の感染者数は？": stat = f"最新の感染者数累計は { numberOfInfected.numberOfInfected() } 件です。"
+    elif userText == "最新の死亡者数は？": stat = f"最新の死亡者数累計は { numberOfDead.numberOfDead() } 件です。"
+    elif userText == "最新の検査数は？": stat = f"最新の検査数累計は { numberOfInspected.numberOfInspected() } 件です。"
+    elif userText == "最新の健康相談件数は？": stat = f"最新の健康相談件数累計は { numberOfGeneral.numberOfGeneral() } 件です。"
+    elif userText == "最新のコロナ相談数は？": stat = f"最新のコロナ相談件数累計は { numberOfCovid.numberOfCovid() } 件です。"
 
     return stat
